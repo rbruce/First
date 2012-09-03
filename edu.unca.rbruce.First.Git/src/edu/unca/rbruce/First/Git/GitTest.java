d1842594cc7b600bcf55fbe9c38001ec04b7dff1
rbruce <bruce@unca.edu>
2012-09-03 18:37:31 -0400

    initial commit

---
+++ edu.unca.rbruce.First.Git/src/edu/unca/rbruce/First/Git/GitTest.java
@@ -0,0 +1,13 @@
+package edu.unca.rbruce.First.Git;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
